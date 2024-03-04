3293b2577cfd6f8806feb13c48a59307eef547f7
CodeOK1993 <koniushenko93@gmail.com>
2024-03-04 10:07:22 +0200

    Metal feature for Mac and AVX for CPU

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,47 @@ extern crate intel_mkl_src;
 
 #[cfg(feature = "accelerate")]
 extern crate accelerate_src;
-mod token_output_stream;
 
-use anyhow::{Error as E, Result};
+mod token_output_stream;
+mod text_generation;
+mod model;
+
+
+use anyhow::{Error as E, Ok, Result, bail};
 use clap::Parser;
 
-use candle_transformers::models::mistral::{Config, Model as Mistral};
-use candle_transformers::models::quantized_mistral::Model as QMistral;
 
-use candle_core::{DType, Device, Tensor};
-use token_output_stream::TokenOutputStream;
+use candle_core::{DType, Device};
 use candle_nn::VarBuilder;
-use candle_transformers::generation::LogitsProcessor;
 use hf_hub::{api::sync::Api, Repo, RepoType};
 use tokenizers::Tokenizer;
 
+use candle_core::quantized::{ggml_file, gguf_file};
 use candle_core::utils::{cuda_is_available, metal_is_available};
 
-pub fn device(cpu: bool) -> Result<Device> {
-    if cpu {
-        Ok(Device::Cpu)
-    } else if cuda_is_available() {
-        Ok(Device::new_cuda(0)?)
-    } else if metal_is_available() {
-        Ok(Device::new_metal(0)?)
-    } else {
-        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-        {
-            println!(
-                "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
-            );
-        }
-        #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
-        {
-            println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
-        }
-        Ok(Device::Cpu)
-    }
-}
+use text_generation::TextGeneration;
+use model::Model;
+
+use candle_transformers::models::mistral::{ Model as Mistral, Config as MistralConfig};
+use candle_transformers::models::quantized_mistral::Model as QMistral;
+use candle_transformers::models::quantized_llama::ModelWeights;
+
+
+
+
 
 /// Loads the safetensors files for a model from the hub based on a json index file.
-pub fn hub_load_safetensors(
+pub fn hub_load_safetensors<P: AsRef<std::path::Path>> (
     repo: &hf_hub::api::sync::ApiRepo,
-    json_file: &str,
-) -> candle_core::Result<Vec<std::path::PathBuf>> {
-    let json_file = repo.get(json_file).map_err(candle_core::Error::wrap)?;
-    let json_file = std::fs::File::open(json_file)?;
+    json_file: P,
+) -> Result<Vec<std::path::PathBuf>> {
+    let json_file = std::fs::File::open(json_file.as_ref())?;
     let json: serde_json::Value =
         serde_json::from_reader(&json_file).map_err(candle_core::Error::wrap)?;
     let weight_map = match json.get("weight_map") {
-        None => candle_core::bail!("no weight map in {json_file:?}"),
+        None => bail!("no weight map in {json_file:?}"),
         Some(serde_json::Value::Object(map)) => map,
-        Some(_) => candle_core::bail!("weight map in {json_file:?} is not a map"),
+        Some(_) => bail!("weight map in {json_file:?} is not a map"),
     };
     let mut safetensors_files = std::collections::HashSet::new();
     for value in weight_map.values() {
@@ -64,116 +53,14 @@ pub fn hub_load_safetensors(
     }
     let safetensors_files = safetensors_files
         .iter()
-        .map(|v| repo.get(v).map_err(candle_core::Error::wrap))
-        .collect::<candle_core::Result<Vec<_>>>()?;
+        .map(|v| repo.get(v) )
+        .collect::<Result<Vec<_>,_>>()?;
     Ok(safetensors_files)
 }
 
 
-enum Model {
-    Mistral(Mistral),
-    Quantized(QMistral),
-}
 
-struct TextGeneration {
-    model: Model,
-    device: Device,
-    tokenizer: TokenOutputStream,
-    logits_processor: LogitsProcessor,
-    repeat_penalty: f32,
-    repeat_last_n: usize,
-}
 
-impl TextGeneration {
-    #[allow(clippy::too_many_arguments)]
-    fn new(
-        model: Model,
-        tokenizer: Tokenizer,
-        seed: u64,
-        temp: Option<f64>,
-        top_p: Option<f64>,
-        repeat_penalty: f32,
-        repeat_last_n: usize,
-        device: &Device,
-    ) -> Self {
-        let logits_processor = LogitsProcessor::new(seed, temp, top_p);
-        Self {
-            model,
-            tokenizer: TokenOutputStream::new(tokenizer),
-            logits_processor,
-            repeat_penalty,
-            repeat_last_n,
-            device: device.clone(),
-        }
-    }
-
-    fn run(&mut self, prompt: &str, sample_len: usize) -> Result<()> {
-        use std::io::Write;
-        self.tokenizer.clear();
-        let mut tokens = self
-            .tokenizer
-            .tokenizer()
-            .encode(prompt, true)
-            .map_err(E::msg)?
-            .get_ids()
-            .to_vec();
-        for &t in tokens.iter() {
-            if let Some(t) = self.tokenizer.next_token(t)? {
-                print!("{t}")
-            }
-        }
-        std::io::stdout().flush()?;
-
-        let mut generated_tokens = 0usize;
-        let eos_token = match self.tokenizer.get_token("</s>") {
-            Some(token) => token,
-            None => anyhow::bail!("cannot find the </s> token"),
-        };
-        let start_gen = std::time::Instant::now();
-        for index in 0..sample_len {
-            let context_size = if index > 0 { 1 } else { tokens.len() };
-            let start_pos = tokens.len().saturating_sub(context_size);
-            let ctxt = &tokens[start_pos..];
-            let input = Tensor::new(ctxt, &self.device)?.unsqueeze(0)?;
-            let logits = match &mut self.model {
-                Model::Mistral(m) => m.forward(&input, start_pos)?,
-                Model::Quantized(m) => m.forward(&input, start_pos)?,
-            };
-            let logits = logits.squeeze(0)?.squeeze(0)?.to_dtype(DType::F32)?;
-            let logits = if self.repeat_penalty == 1. {
-                logits
-            } else {
-                let start_at = tokens.len().saturating_sub(self.repeat_last_n);
-                candle_transformers::utils::apply_repeat_penalty(
-                    &logits,
-                    self.repeat_penalty,
-                    &tokens[start_at..],
-                )?
-            };
-
-            let next_token = self.logits_processor.sample(&logits)?;
-            tokens.push(next_token);
-            generated_tokens += 1;
-            if next_token == eos_token {
-                break;
-            }
-            if let Some(t) = self.tokenizer.next_token(next_token)? {
-                print!("{t}");
-                std::io::stdout().flush()?;
-            }
-        }
-        let dt = start_gen.elapsed();
-        if let Some(rest) = self.tokenizer.decode_rest().map_err(E::msg)? {
-            print!("{rest}");
-        }
-        std::io::stdout().flush()?;
-        println!(
-            "\n{generated_tokens} tokens generated ({:.2} token/s)",
-            generated_tokens as f64 / dt.as_secs_f64(),
-        );
-        Ok(())
-    }
-}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -218,10 +105,13 @@ struct Args {
     tokenizer_file: Option<String>,
 
     #[arg(long)]
-    weight_files: Option<String>,
+    model_path: Option<String>,
 
     #[arg(long)]
     quantized: bool,
+
+    #[arg(long)]
+    llama: bool,
 
     /// Penalty to be applied for repeating tokens, 1. means no penalty.
     #[arg(long, default_value_t = 1.1)]
@@ -230,6 +120,109 @@ struct Args {
     /// The context size to consider for the repeat penalty.
     #[arg(long, default_value_t = 64)]
     repeat_last_n: usize,
+}
+
+impl Args {
+
+    fn tokenizer(&self) -> Result<Tokenizer> {
+        let tokenizer_path = match &self.tokenizer_file {
+            Some(config) => std::path::PathBuf::from(config),
+            None => {
+                let api = hf_hub::api::sync::Api::new()?;
+                let repo = "mistralai/Mistral-7B-v0.1";
+                let api = api.model(repo.to_string());
+                api.get("tokenizer.json")?
+            }
+        };
+        Tokenizer::from_file(tokenizer_path).map_err(E::msg)
+    }
+
+    fn device(&self) -> Result<Device> {
+        if self.cpu {
+            Ok(Device::Cpu)
+        } else if cuda_is_available() {
+            Ok(Device::new_cuda(0)?)
+        } else if metal_is_available() {
+            Ok(Device::new_metal(0)?)
+        } else {
+            #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+            {
+                println!(
+                    "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
+                );
+            }
+            #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
+            {
+                println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
+            }
+            Ok(Device::Cpu)
+        }
+    }
+
+    fn model(&self, device: &Device) -> Result<Model> {
+        let api = Api::new()?;
+        let model_id = match &self.model_id {
+            Some(model_id) => model_id,
+            None => {
+                if self.quantized {
+                    "DanielClough/Candle_Mistral-7B-Instruct-v0.2"
+                } else {
+                    "mistralai/Mistral-7B-v0.1"
+                }
+            }
+        };
+        let repo = api.repo(Repo::with_revision(
+            model_id.to_string(),
+            RepoType::Model,
+            self.revision.clone(),
+        ));
+    
+        let model_path = match &self.model_path {
+            Some(file) => repo.get(file)?,
+            None => {
+                if self.quantized {
+                    repo.get("Candle_Mistral-7B-Instruct-v0.2_q4k.gguf")?
+                } else {
+                    repo.get("model.safetensors.index.json")?
+                }
+            }
+        };      
+
+        let config: MistralConfig = MistralConfig::config_7b_v0_1(self.use_flash_attn);
+        let model = if self.quantized {
+            if self.llama {
+                let mut file = std::fs::File::open(&model_path)?;
+                let model = match model_path.extension().and_then(|v| v.to_str()) {
+                    Some("gguf") => {
+                        let model = gguf_file::Content::read(&mut file).map_err(|e| e.with_path(model_path))?;
+                        ModelWeights::from_gguf(model, &mut file, device)?
+                    }
+                    Some("ggml" | "bin") | Some(_) | None => {
+                        let model = ggml_file::Content::read(&mut file, device)
+                            .map_err(|e| e.with_path(model_path))?;
+                        ModelWeights::from_ggml(model, 8)?
+                    }
+                };
+                Model::LlmaMistral(model)       
+            } else {            
+                let vb =
+                    candle_transformers::quantized_var_builder::VarBuilder::from_gguf(&model_path, device)?;
+                let model = QMistral::new(&config, vb)?;
+                Model::QuantizedMistral(model)
+            }
+        } else {
+            let dtype = if device.is_cuda() {
+                DType::BF16
+            } else {
+                DType::F32
+            };
+            let safetensors = hub_load_safetensors(&repo, &model_path)?;
+            let vb = unsafe { VarBuilder::from_mmaped_safetensors(&safetensors, dtype, device)? };
+            let model = Mistral::new(&config, vb)?;
+            Model::Mistral(model)
+        };
+        Ok(model)
+    }
 }
 
 fn main() -> Result<()> {
@@ -245,7 +238,7 @@ fn main() -> Result<()> {
         None
     };
     println!(
-        "cude: {}, avx: {}, neon: {}, simd128: {}, f16c: {}",
+        "cuda: {}, avx: {}, neon: {}, simd128: {}, f16c: {}",
         candle_core::utils::cuda_is_available(),
         candle_core::utils::with_avx(),
         candle_core::utils::with_neon(),
@@ -260,62 +253,12 @@ fn main() -> Result<()> {
     );
 
     let start = std::time::Instant::now();
-    let api = Api::new()?;
-    let model_id = match args.model_id {
-        Some(model_id) => model_id,
-        None => {
-            if args.quantized {
-                "lmz/candle-mistral".to_string()
-            } else {
-                "mistralai/Mistral-7B-v0.1".to_string()
-            }
-        }
-    };
-    let repo = api.repo(Repo::with_revision(
-        model_id,
-        RepoType::Model,
-        args.revision,
-    ));
-    let tokenizer_filename = match args.tokenizer_file {
-        Some(file) => std::path::PathBuf::from(file),
-        None => repo.get("tokenizer.json")?,
-    };
-    let filenames = match args.weight_files {
-        Some(files) => files
-            .split(',')
-            .map(std::path::PathBuf::from)
-            .collect::<Vec<_>>(),
-        None => {
-            if args.quantized {
-                vec![repo.get("model-q4k.gguf")?]
-            } else {
-                hub_load_safetensors(&repo, "model.safetensors.index.json")?
-            }
-        }
-    };
+    let tokenizer = args.tokenizer().map_err(E::msg)?;
     println!("retrieved the files in {:?}", start.elapsed());
-    let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
 
     let start = std::time::Instant::now();
-    let config = Config::config_7b_v0_1(args.use_flash_attn);
-    let device = device(args.cpu)?;
-    let (model, device) = if args.quantized {
-        let filename = &filenames[0];
-        let vb =
-            candle_transformers::quantized_var_builder::VarBuilder::from_gguf(filename, &device)?;
-        let model = QMistral::new(&config, vb)?;
-        (Model::Quantized(model), device)
-    } else {
-        let dtype = if device.is_cuda() {
-            DType::BF16
-        } else {
-            DType::F32
-        };
-        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&filenames, dtype, &device)? };
-        let model = Mistral::new(&config, vb)?;
-        (Model::Mistral(model), device)
-    };
-
+    let device = args.device()?;
+    let model = args.model(&device)?;
     println!("loaded the model in {:?}", start.elapsed());
 
     let mut pipeline = TextGeneration::new(
@@ -328,6 +271,6 @@ fn main() -> Result<()> {
         args.repeat_last_n,
         &device,
     );
-    pipeline.run(&args.prompt, args.sample_len)?;
+    pipeline.run(args.prompt, args.sample_len)?;
     Ok(())
 }
