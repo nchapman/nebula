317e8a9b51ec760e953a88adbb640a64b183e190
iKachaykin <ivankachaykin707@gmail.com>
2024-03-20 20:43:12 +0200

    Unify paths + use a sample provided by default on GitHub

--- examples/whisper.rs
+++ examples/whisper.rs
@@ -8,14 +8,14 @@ fn main() {
     let model_options = ModelOptions::default().with_n_gpu_layers(10);
 
     let mut model = Model::new(
-        "/home/ivan/Projects/nebula/models/ggml-base.en.bin", model_options)
+        "models/ggml-base.en.bin", model_options)
         .unwrap();
 
     let predict_options = PredictOptions::default().with_n_len(150);
 
     model
         .predict(
-            "/home/ivan/Projects/nebula/samples/nata_1.wav".into(),
+            "samples/jfk.wav".into(),
             predict_options,
             Box::new(|token| {
                 print!("{}", token);
