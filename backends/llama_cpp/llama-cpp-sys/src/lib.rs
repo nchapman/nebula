25ad74d890d828e58558fc567dda061ef752cd6e
Warefoxe <jurassic151@gmail.com>
2024-06-21 19:56:18 +0200

    add embed

--- backends/llama_cpp/llama-cpp-sys/src/lib.rs
+++ backends/llama_cpp/llama-cpp-sys/src/lib.rs
@@ -65,7 +65,7 @@ impl DeviceInfo {
         vars.iter()
             .filter(|v| {
                 if self.library == "cpu" {
-                    self.variant <= CPUCapability::from(&v.variant)
+                    v.variant == "cpu" && self.variant <= CPUCapability::from(&v.variant)
                 } else {
                     self.library == v.library || v.library == "cpu"
                 }
