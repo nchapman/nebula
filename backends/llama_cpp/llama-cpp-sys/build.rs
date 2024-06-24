e996bbcbf8990cc8c136199cdbf9ccb80977c16b
Warefoxe <jurassic151@gmail.com>
2024-06-24 15:31:45 +0200

    update platform specific deps

--- backends/llama_cpp/llama-cpp-sys/build.rs
+++ backends/llama_cpp/llama-cpp-sys/build.rs
@@ -92,7 +92,6 @@ mod macos {
         } else {
             "Debug".to_string()
         };
-        println!("cargo:warning=Installing binaries to dist dir {build_dir}");
         println!("cargo:warning=Installing binaries to dist dir {dist_dir}");
         std::fs::create_dir_all(dist_dir).expect("can`t create dist directory");
         for entry in glob::glob(&format!("{build_dir}/build/*/*.dylib"))
@@ -100,6 +99,7 @@ mod macos {
         {
             if let Ok(path) = entry {
                 let path = path.into_os_string().into_string().unwrap();
+                println!("{path}");
                 std::process::Command::new("cp")
                     .arg(path)
                     .arg(dist_dir)
