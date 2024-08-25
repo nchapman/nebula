da9d182c6bd006e25b05ae244ac2fb9e6096ef52
iKachaykin <ivankachaykin707@gmail.com>
2024-08-25 18:22:31 +0300

    Add building dependencies for macos into build.sh script

--- build.sh
+++ build.sh
@@ -47,8 +47,47 @@ build_dependencies_for_ubuntu() {
     echo "Successfully built dependencies for Ubuntu"
 }
 
+build_dependencies_for_mac_os() {
+    brew install llvm
+    if [ $? -ne 0 ]; then
+        echo "Failed to install clang"
+        exit 1
+    fi
+
+    brew install espeak-ng
+    if [ $? -ne 0 ]; then
+        echo "Failed to install espeak-ng"
+        exit 1
+    fi
+
+    mkdir -p ./libtorch/
+    curl -o ./libtorch/libtorch-2.0.0.zip https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.0.0%2Bcpu.zip
+    if [ $? -ne 0 ]; then
+        echo "Failed to download libtorch"
+        exit 1
+    fi
+
+    brew install unzip
+    if [ $? -ne 0 ]; then
+        echo "Failed to install unzip"
+        exit 1
+    fi
+
+    unzip ./libtorch/libtorch-2.0.0.zip -d ./libtorch/
+    if [ $? -ne 0 ]; then
+        echo "Failed to unzip libtorch"
+        exit 1
+    fi
+
+    export LIBTORCH=$(cd "$(dirname "./libtorch/")" && pwd)/$(basename "./libtorch/")
+    cd -
+    export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
+
+    echo "Successfully built dependencies for MacOS"
+}
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Running on macOS"
+    build_dependencies_for_mac_os
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     distribution=$(get_linux_distribution)
     if [ "$distribution" == "ubuntu" ]; then
